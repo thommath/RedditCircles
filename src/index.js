85ef29fd5a0af7639ae5a2add84a1bbd8e1b2790
Thomas <thomlmath@gmail.com>
2019-08-19 14:59:38 +0200

    Some optimizations and resize listener

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ function initThree() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
-  
 
   const renderer = new THREE.WebGLRenderer({canvas});
 
@@ -29,15 +28,6 @@ function initThree() {
 
   const scene = new THREE.Scene();
 
-/*
-  {
-    const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
-    scene.add(light);
-  }*/
-
   {
     var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
     scene.add(light);
@@ -45,21 +35,42 @@ function initThree() {
 
   renderer.render(scene, camera);
 
+  // Mouse listeners
   const mouseHandler = new MouseHandler(scene, camera);
+
+
+  // Window resize
+  window.addEventListener( 'resize', () => {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    
+    camera.updateProjectionMatrix();
+    
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+  }, false);
 
 
   return { scene, renderer, camera, canvas, mouseHandler };
 }
 
+
+
 function main() {
+
+  // Temp variables
+  const tempVec = new THREE.Vector3();
+
 
   const { scene, renderer, camera, mouseHandler } = initThree();
 
-  // Mouse listeners
-//  const mouseHandler = new MouseHandler(scene, camera);
   const screenWidth = window.innerWidth*0.002;
   const screenHeight = window.innerHeight*0.002;
 
+
+  // Generate all the circles
   const circles = [];
 
   for(let i = 0; i < 50; i += 1) {
@@ -86,7 +97,7 @@ function main() {
   
   let lastTime = 0;
   function render(time) {
-    const dt = (time - lastTime);
+    const dt = (time - lastTime)*0.05;
     lastTime = time;
 
     while(lines.length) scene.remove(lines.pop());
@@ -94,74 +105,79 @@ function main() {
     circles.forEach((circle, i) => {
 
       // Move the circles
-      circle.mesh.translateX(circle.vx);
-      circle.mesh.translateY(circle.vy);
+      circle.mesh.translateX(circle.vx*dt);
+      circle.mesh.translateY(circle.vy*dt);
 
       // Wrap the screen
+      // Using translate to maintain the offset over the border in case of serious lagg
       if (circle.mesh.position.y > screenHeight/2) {
-        circle.mesh.position.setY(-screenHeight/2);
+        circle.mesh.translateY(-screenHeight);
       }
       if (circle.mesh.position.y < -screenHeight/2) {
-        circle.mesh.position.setY(screenHeight/2);
+        circle.mesh.translateY(screenHeight);
       }
       if (circle.mesh.position.x > screenWidth/2) {
-        circle.mesh.position.setX(-screenWidth/2);
+        circle.mesh.translateX(-screenWidth);
       }
       if (circle.mesh.position.x < -screenWidth/2) {
-        circle.mesh.position.setX(screenWidth/2);
+        circle.mesh.translateX(screenWidth);
       }
 
       // Draw lines between close circles
-      circles.forEach((c2, i2) => {
-        if (i >= i2) return;
+      for (let i2 = i+1; i2 < circles.length; i2 += 1) {
+        const c2 = circles[i2];
 
         const dist = circle.mesh.position.distanceTo(c2.mesh.position);
 
         if (dist < 0.2) {
 
+          let geo;
+          let material;
+          // If line exists update points and line width
           if (circle.lines[i2]) {
-            while (circle.lines[i2].geometry.vertices.length) circle.lines[i2].geometry.vertices.pop();
-
-            circle.lines[i2].geometry.setFromPoints([circle.mesh.position, c2.mesh.position]);
-            circle.lines[i2].geometry.verticesNeedUpdate = true;
-
-          } else {
-            const geo = new THREE.Geometry();
             
-            geo.vertices.push(circle.mesh.position.clone());
-            geo.vertices.push(c2.mesh.position.clone());
-  
-  
+            geo = circle.lines[i2].geometry;
+            material = circle.lines[i2].material;
+            geo.verticesNeedUpdate = true;
+            
+          } else {
+
+            geo = new THREE.Geometry();
+            material = new THREE.LineBasicMaterial( { color: 0xaaaaaa } );
             const line = new THREE.Line(
               geo,
-              new THREE.LineBasicMaterial( { color: 0xaaaaaa, linewidth: 5-Math.ceil(dist*20) } )
-            )
-            scene.add( line );
+              material
+            );
             
+            scene.add( line );   
             circle.lines[i2] = line;
           }
+
+          geo.setFromPoints([circle.mesh.position, c2.mesh.position]);
+          material.linewidth = 5-Math.ceil(dist*20);
 
         } else {
           if (circle.lines[i2]) {
             scene.remove(circle.lines[i2]);
-            delete circle.lines[i2];
+            circle.lines[i2] = null;
           }
         }
 
-      });
+      };
 
       // Affect circle velocity with mouse position
       const mouse = mouseHandler.get3DMousePosition();
       circles.forEach(circle => {
 
-        const nVec = new THREE.Vector3();
-        nVec.subVectors(mouse, circle.mesh.position);
+        tempVec.subVectors(mouse, circle.mesh.position);
         
-        if (Math.pow(nVec.x, 2) + Math.pow(nVec.y, 2) < 0.01) {
+        if (Math.pow(tempVec.x, 2) + Math.pow(tempVec.y, 2) < 0.01) {
 
-          circle.vx += (0.01-nVec.x) * 0.0001;
-          circle.vy += (0.01-nVec.y) * 0.0001;
+          // Accelerate the circles away from the mouse
+          circle.vx += (0.01-tempVec.x) * 0.0001;
+          circle.vy += (0.01-tempVec.y) * 0.0001;
   
+          // Limit the top velocity
           circle.vx = Math.min(0.003, circle.vx);
           circle.vy = Math.min(0.003, circle.vy);
           circle.vx = Math.max(-0.003, circle.vx);
